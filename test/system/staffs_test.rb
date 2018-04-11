3c8c9f3101f95faad4edb694f5357c3f76a93142
Musomoka <musosuma@gmail.com>
2018-04-11 12:08:03 +0200

    removed staff scaffold

--- test/system/staffs_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class StaffsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit staffs_url
-  #
-  #   assert_selector "h1", text: "Staff"
-  # end
-end
