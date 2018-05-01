347447436abd7b7e09413995fa738572b625ec26
Musomoka <musosuma@gmail.com>
2018-05-01 14:06:03 +0200

    add seeds for production env

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }"," { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke'"," movie: movies.first)
 
+case Rails.env
+when "development"
+  
 User.create(email: 'user@example.com', password: 'password')
 
 
@@ -40,4 +43,26 @@ end
 	)
 end
 
+
+when "production"
+	payment_nodes= ['emergency fund', 'saving', 'utility', 'healthcare', 'Credit Cards and Debt','food & Groceries', 'personal care', 'entertainment', 'transportation']
+	reciepts_nodes= ['salary', 'business income', 'other income']
+
+		reciepts = Category.create(name: 'Reciepts')
+
+		payments = Category.create(name: 'payments')
+
+		reciepts_nodes.map do |c,parent|
+
+			Category.create(name: c.humanize, parent: reciepts)
+
+		end
+
+		payment_nodes.map do |c,parent|
+
+			Category.create(name: c.humanize, parent: payments)
+
+		end
+
+end
 
